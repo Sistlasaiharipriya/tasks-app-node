ea676c1671490a36491c4073c9226e7568496b7c
Haripriya <sistlasaiharipriya@gmail.com>
2024-10-04 07:04:40 +0530

    running on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //tasks app
+const express = require("express");
+const app = express();
+
+// Start the server
+app.listen(3000, () => {
+    console.log(`Task API listening at http://localhost:${port}`);
+  });
