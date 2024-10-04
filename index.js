1ab330f69c2eae3452a307d0af061279be7d930f
Haripriya <sistlasaiharipriya@gmail.com>
2024-10-04 19:04:52 +0530

    create and get tasks

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ const app = express();
 // Start the server
 app.listen(3000, () => {
     console.log(`Task API listening at http://localhost:${port}`);
+});
+
+//or using json 
+app.use(express.json());
+
+//task array
+let tasks = [];
+
+//get all tasks
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+
+//create a new task
+app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+    };
+    tasks.push(newTask);
+    res.status(201).json({
+      status:"created successfully"
+    });
   });
